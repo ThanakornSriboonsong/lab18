981606cd63d7d9f41f3fd02f569e3e12fd8a489c
Pleum <plemplemplemplem5@gmail.com>
2025-02-11 19:57:38 +0700

    done

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,26 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *a , Rect *b){
+    double  tl , tr , wi , hei , up , low ;
+    tr = min(a->x+a->w,b->x+b->w) ;
+	tl = max(a->x,b->x) ;
+    up = min(a->y,b->y) ;
+	low = max(b->y-b->h,a->y-a->h) ;  
+	
+	wi = tr-tl;
+	hei = up-low;
+    if(wi>0 and hei>0) return wi*hei ;
+	else return 0 ;
+
+
+
+
 
 
 
 }
+    
+
+
+
